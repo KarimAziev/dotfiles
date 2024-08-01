666769882435a018d92286d3055ffbecc772045c
KarimAziev <karim.aziiev@gmail.com>
2024-08-01 21:02:10 +0300

    feat: Add dependencies installation to init_pyenv function

--- install.sh
+++ install.sh
@@ -448,6 +448,12 @@ init_pyenv() {
       echo ''
     } >> ~/.bashrc
   fi
+
+  sudo apt-get install -y \
+    make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl llvm \
+    libncurses5-dev libncursesw5-dev xz-utils tk-dev \
+    libffi-dev liblzma-dev python-openssl git
 }
 
 # Init GHCup the main installer for Haskell
